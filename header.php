fca136bac86cd546958e7a242fea50747c6b2b25
Christian Frugard <cfrugard@lightower.com>
2017-03-03 09:36:08 -0500

    extracting header and footer on index.php page

---
+++ header.php
@@ -0,0 +1,9 @@
+<header id="header">
+  <nav class="left">
+    <a href="#menu"><span>Menu</span></a>
+  </nav>
+  <a href="index.html" class="logo">Sandy's Pet Shop</a>
+  <nav class="right"><a href="form.html" class="button alt">Contact Us!</a>
+
+  </nav>
+</header>
--- index.php
+++ index.php
@@ -10,15 +10,10 @@
 	<body>
 
 		<!-- Header -->
-			<header id="header">
-				<nav class="left">
-					<a href="#menu"><span>Menu</span></a>
-				</nav>
-				<a href="index.html" class="logo">Sandy's Pet Shop</a>
-				<nav class="right"><a href="form.html" class="button alt">Contact Us!</a>
-					
-				</nav>
-			</header>
+
+		<?php
+			include('header.php');
+		?>
 
 		<!-- Menu -->
 			<nav id="menu">
@@ -28,7 +23,7 @@
 					<li><a href="grooming.html">Grooming</a></li>
 				</ul>
 				<ul class="actions vertical">
-					
+
 				</ul>
 			</nav>
 
@@ -122,31 +117,8 @@
 			</section>
 
 		<!-- Footer -->
-			<footer id="footer">
-				<div class="inner">
-					<h2>Call Today</h2>
-					<ul class="actions">
-						<li><span class="icon fa-phone"></span> <a href="tel:+9086075309">(908) 607-5309</a></li>
-						<li><span class="icon fa-envelope"></span> <a href="mailto:sandyspetshop@gmail.com">sandyspetshop@gmail.com</a></li>
-						<li><span class="icon fa-map-marker"></span> 108 WeLovePets Road, Holmdel, NJ 07733</li>
-						<li><span class="icon fa-info-circle"></span> Hours of Operation: M-F 9am - 8pm, Sat-Sun 9am-5pm</li>
-						<li><span class="icon fa-briefcase"></span> <a href="http://www.veterinarians.com">Visit your local vet!</a></li>
-						<li><span class="icon fa-hospital-o"></span> <a href="http://www.dogsonthefarm.com">Need a kennel?</a></li>
-					</ul>
-				</div>
-				<div class="copyright">
-					&copy; Sandy's Pet Shop.
-				</div>
-			</footer>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-			<script src="jquery-2.1.0.min.js"></script>
-	<script src="app.js"></script>
-
+		<?php
+			include('footer.php');
+		?>
 	</body>
 </html>
